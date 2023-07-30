02d160ace82aa43d93b23b3e6ff4d71839d684cf
ShortCu7 <87804260+thaR00t@users.noreply.github.com>
2023-07-30 16:49:32 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,1 @@
-#/bin/bash
 
-
-sudo tr -d '\r' <ar-tool.py> ar-tool-linux.py
-
-sudo chmod +x ar-tool-linux.py
-sudo mv ar-tool-linux.py ar-tool
-sudo mv ar-tool /usr/bin/
-
-echo "Installation Done..."
-
-clear
-
-ar-tool --help
